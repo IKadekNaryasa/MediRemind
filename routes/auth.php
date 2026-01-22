26177e8f1241f9d534c428e10f90ceef3c94cbcb
IKadekNaryasa <qy.naryasa@gmail.com>
2026-01-22 13:20:07 +0800

    menyesuaikan routes

--- routes/auth.php
+++ routes/auth.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('GMR')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    Route::get('gmr-register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
